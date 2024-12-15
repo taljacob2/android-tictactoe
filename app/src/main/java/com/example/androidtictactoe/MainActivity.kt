3180203a0eab753cbfe4693266e30a466ba7ab8b
Tal Jacob <taljacob2@gmail.com>
2024-12-15 08:23:36 +0200

    Test Activity Button

--- app/src/main/java/com/example/androidtictactoe/MainActivity.kt
+++ app/src/main/java/com/example/androidtictactoe/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.androidtictactoe
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupUi()
+    }
+
+    private fun setupUi() {
+        val button: Button = findViewById(R.id.buttonMain)
+        button.setOnClickListener { finish() }
     }
 }
